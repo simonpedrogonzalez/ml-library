b7854d75771dc58db9835737f262e876bd3e77cf
Simón Pedro <simon.pedro.g@gmail.com>
2024-10-10 11:28:52 -0600

    update run

--- run_h2.sh
+++ run_h2.sh
@@ -8,7 +8,20 @@ python3 ensemble_learning/h2e2a_adaboost.py
 echo "Exercise 2b Bagged Trees"
 python3 ensemble_learning/h2e2b_bagged_trees.py
 
+# Exercise 2c
+echo "Exercise 2c Bagged Trees Bias and Variance"
+python3 ensemble_learning/h2e2c_bagged_trees_advantage.py
+
 # Exercise 2d
 echo "Exercise 2d Random Forest"
 python3 ensemble_learning/h2e2d_random_forest.py
 
+# Exercise 2e
+echo "Exercise 2e Random Forest Bias and Variance"
+python3 ensemble_learning/h2e2e_random_forest_advantage.py
+
+# Exercise 3
+echo "Exercise 3 Credit Card Default dataset"
+python3 ensemble_learning/h2e3_credit_card_default.py
+
+# Exercise 4
