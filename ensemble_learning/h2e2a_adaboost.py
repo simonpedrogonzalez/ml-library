9231e2a29ca48ecc26b5397a401922dff721f6da
Simón Pedro <simon.pedro.g@gmail.com>
2024-10-17 00:51:58 -0600

    add single learner weighted train error report

--- ensemble_learning/h2e2a_adaboost.py
+++ ensemble_learning/h2e2a_adaboost.py
@@ -6,10 +6,12 @@ from utils.preprocessing import dataset_to_cat_encoded_dataset, transform_num_to
 from utils.stats import avg_error
 from decision_tree.fast_id3 import FastID3
 from ensemble_learning.adaboost import AdaBoost
+import numpy as np
 
 
 def train_test_run(adaboost, data, n):
     train, train_labels, test, test_labels = data.train, data.train_labels, data.test, data.test_labels
+    last_w = adaboost.w
     if n == 1:
         adaboost.fit(train, train_labels)
         train_pred = adaboost.predict(train)
@@ -23,11 +25,17 @@ def train_test_run(adaboost, data, n):
     test_error = avg_error(test_pred, test_labels.values)
     return train_error, test_error
 
-def unweighted_errors_of_each_learner(adaboost, data):
+def errors_of_each_learner(adaboost, data, ws):
     train, train_labels, test, test_labels = data.train, data.train_labels, data.test, data.test_labels
     learners = adaboost.trained_learners
     train_errors = []
     test_errors = []
+    w_train_errors = []
+    w_test_errors = []
+
+    def w_error(y, y_pred, w):
+        return np.sum(w * (y != y_pred))
+
     for i, learner in enumerate(learners):
         train_pred = learner.predict(train)
         test_pred = learner.predict(test)
@@ -35,11 +43,17 @@ def unweighted_errors_of_each_learner(adaboost, data):
         test_error = avg_error(test_pred, test_labels.values)
         train_errors.append(train_error)
         test_errors.append(test_error)
-    return train_errors, test_errors
+        w_train_error = w_error(train_labels.values, train_pred, ws[i])
+        w_test_error = w_error(test_labels.values, test_pred, ws[i])
+        w_train_errors.append(w_train_error)
+        w_test_errors.append(w_test_error)
+
+    return train_errors, test_errors, w_train_errors, w_test_errors
 
 def report(data):
     max_n = 500
     results = []
+    ws = []
 
     adaboost = AdaBoost(FastID3('infogain', 1), 1)
     for n in range(1, max_n + 1):
@@ -49,17 +63,23 @@ def report(data):
         progress = n / max_n
         print(f"n: {n}, Train Error: {round(train_error, 3)}, Test Error: {round(test_error, 3)}, Time: {et}s, Progress: {progress * 100:.2f}%")
         results.append([n, train_error, test_error])
+        ws.append(adaboost.w)
 
     print("Exporting report for exercise 2a...")
     df = pd.DataFrame(results, columns=['n', 'train_error', 'test_error'])
     df.to_csv('ensemble_learning/reports/h2e2a_1st_report.csv', index=False)
 
+    ws = [[0] * len(ws[0])] + ws[:-1]
+    ws = np.array(ws)
+
     print("Evaluating single learner errors...")
-    train_errors, test_errors = unweighted_errors_of_each_learner(adaboost, data)
+    train_errors, test_errors, w_train_errors, w_test_errors = errors_of_each_learner(adaboost, data, ws)
     learners = list(range(1, len(train_errors) + 1))
     print("Exporting single learner errors...")
     df = pd.DataFrame({'t': learners, 'train_error': train_errors, 'test_error': test_errors})
     df.to_csv('ensemble_learning/reports/h2e2a_2nd_report.csv', index=False)
+    df = pd.DataFrame({'t': learners, 'train_error': w_train_errors, 'test_error': w_test_errors})
+    df.to_csv('ensemble_learning/reports/h2e2a_4th_report.csv', index=False)
     
     # single tree run
     print("Evaluating single tree run...")
@@ -72,6 +92,8 @@ def report(data):
     print(f"Train Error: {round(train_error, 3)}, Test Error: {round(test_error, 3)}")
     df = pd.DataFrame([[train_error, test_error]], columns=['train_error', 'test_error'])
     df.to_csv('ensemble_learning/reports/h2e2a_3rd_report.csv', index=False)
+
+    
 
 data = bank_dataset()
 data.train = transform_num_to_bin_median(data.train)
