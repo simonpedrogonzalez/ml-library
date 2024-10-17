e36bb66a6514113e639be3b812cc0a479b8b9520
Simón Pedro <simon.pedro.g@gmail.com>
2024-10-17 00:53:23 -0600

    linear regression plots

--- linear_regression/reports/plots.py
+++ linear_regression/reports/plots.py
@@ -3,4 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plo
+all_df = pd.read_csv('linear_regression/reports/h2e4_report.csv')
+
+df = all_df[all_df['model'] == 'Batch Gradient Descent']
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.lineplot(x='n_iter', y='train_error', data=df, ax=ax, label='Cost Function')
+# sns.lineplot(x='n_iter', y='test_error', data=df, ax=ax, label='Test Error')
+ax.set_xlabel('n')
+ax.set_ylabel('Error')
+ax.set_title('Train and Test Error of Batch Gradient Descent')
+ax.legend()
+plt.savefig('linear_regression/reports/h2e4a_plot.png')
+
+df = all_df[all_df['model'] == 'Stochastic Gradient Descent']
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.lineplot(x='n_iter', y='train_error', data=df, ax=ax, label='Cost Function')
+# sns.lineplot(x='n_iter', y='test_error', data=df, ax=ax, label='Test Error')
+ax.set_xlabel('n')
+ax.set_ylabel('Error')
+ax.set_title('Train and Test Error of Stochastic Gradient Descent')
+ax.legend()
+plt.savefig('linear_regression/reports/h2e4b_plot.png')
